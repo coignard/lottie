45e9345f339b33d86d54e6409b720680372aa888
René Coignard <contact@renecoignard.com>
2026-03-08 19:46:05 +0100

    chore: add license notice to source code

--- src/app.rs
+++ src/app.rs
@@ -1,3 +1,20 @@
+// This file is part of Lottie.
+//
+// Copyright (c) 2026  René Coignard <contact@renecoignard.com>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 use std::{collections::HashSet, fs, io, path::PathBuf};
 
 use ratatui::{
@@ -2026,7 +2043,7 @@ For very, very, very long notes.
 
 = And this is a synopsis.
 
-INT. EDEKA - EVENING
+INT. EDEKA - ABEND
 
 Unlike Beat, there's no full render or PDF export here, but you can always save your screenplay and open it in Beat to do that. In Beat, synopses wouldn't appear in the rendered script, nor would comments. Which is why they share the same colour here, incidentally.
 
